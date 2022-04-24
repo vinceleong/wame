1211cbfbbbb9a031373624851605c2d6c892bebb
chunyong-jagaapp <chunyong@jagaapp.com>
2022-04-24 09:23:09 +0800

    Added google url console files

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="scroll-smooth">
-        <Head></Head>
+        <Head>
+          <meta
+            name="google-site-verification"
+            content="ISq8IpU9RM48UUBfTdITLF_rYefjUWwWP_bUwsgwJXs"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
